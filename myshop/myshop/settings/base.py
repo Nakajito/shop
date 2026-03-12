dbc50c8e13b5b33592785e1c744555c42caf9747
monse <tejadamonserratt@gmail.com>
2026-03-11 21:48:52 -0600

    jalando-mayorista

--- myshop/myshop/settings/base.py
+++ myshop/myshop/settings/base.py
@@ -107,7 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
