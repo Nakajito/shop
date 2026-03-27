27647ab979c26f7e913b02df01f01a0da4a71f9d
Daniel <nakajito@proton.me>
2026-03-26 22:14:21 -0500

    chore(static): Agrega configuración para servir archivos estáticos y media en desarrollo

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 """
 Main URL configuration for the 'myshop' project.
@@ -43,3 +43,13 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
