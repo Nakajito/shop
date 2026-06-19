58db3097349b9c18857af2f7fafcbcba6e71acab
Daniel <nakajito@proton.me>
2026-06-18 19:18:59 -0600

    fix: import re

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -20,6 +20,8 @@ Static & media:
     (nginx/Caddy) in front of MEDIA_ROOT, or using cloud storage.
 """
 
+import re
+
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
